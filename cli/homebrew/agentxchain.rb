b05834531cfe20c04ddde8910656b27ff470fd11
Shivam Tiwari <shivamtiwari1011@gmail.com>
2026-04-06 10:13:16 -0400

    Sync homebrew mirror for v2.13.0

--- cli/homebrew/agentxchain.rb
+++ cli/homebrew/agentxchain.rb
@@ -2,7 +2,7 @@ class Agentxchain < Formula
   desc "CLI for AgentXchain governed multi-agent software delivery"
   homepage "https://agentxchain.dev"
   url "https://registry.npmjs.org/agentxchain/-/agentxchain-2.13.0.tgz"
-  sha256 "18ce86d3d2262141da50e3a4d99461562cbf63d1e0e52d35abf8fc283c06ce3f"
+  sha256 "99fcb68e8ceff62be6e964fae9e1fdd0e119b650afd35ee199d108ef8eef0e74"
   license "MIT"
 
   depends_on "node"
