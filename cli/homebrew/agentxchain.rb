41c38ceb1569921fad31bcb7251f3c25104bd521
Shivam Tiwari <shivamtiwari1011@gmail.com>
2026-04-14 23:08:17 -0400

    2.93.0

--- cli/homebrew/agentxchain.rb
+++ cli/homebrew/agentxchain.rb
@@ -1,7 +1,7 @@
 class Agentxchain < Formula
   desc "CLI for AgentXchain governed multi-agent software delivery"
   homepage "https://agentxchain.dev"
-  url "https://registry.npmjs.org/agentxchain/-/agentxchain-2.92.0.tgz"
+  url "https://registry.npmjs.org/agentxchain/-/agentxchain-2.93.0.tgz"
   sha256 "dec93beb52d375ff6c89774f18f53a1760a115c44bd29caf9a6e63da6c5590d1"
   license "MIT"
 
